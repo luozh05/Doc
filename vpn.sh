560661045ae30e4c745b19eecdf6558051d9bd1c
zhanhong luo <zhanhong.luo@nio.com>
2018-04-21 17:44:59 +0800

    add vpn

---
+++ vpn.sh
@@ -0,0 +1,9 @@
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# File Name:/home/zhanhongluo/luoDoc/vpn.sh
+# Author:luozh05
+# mail: luozh05@163.com
+# Created Time:2018年03月13日 星期二 10时41分21秒
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#!/bin/bash
+
+sudo openconnect -u zhanhong.luo --no-http-keepalive --authgroup="NIO(RSA)" vpncn1.nevext.com
